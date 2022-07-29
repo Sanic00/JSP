a1114014b8765c2f8e8d35ac072a679983d90b8e
JaeYeon Kim <105400221+Sanic00@users.noreply.github.com>
2022-07-29 15:33:03 +0900

    Add files via upload

--- Jspworkspace/ServletProject/src/main/java/com/test/ContextFileServlet.java
+++ Jspworkspace/ServletProject/src/main/java/com/test/ContextFileServlet.java
@@ -14,14 +14,28 @@ public class ContextFileServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		                   //경로를 지정해준다
-		String readFile = "/WEB-INF/testFile.txt";
+		String readFile = "/WEB-INF/textFile.txt";
 		
 	    InputStream is = getServletContext().getResourceAsStream(readFile);
 	    
 	    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 	    
-	    
+	    response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
 		
+		
+		out.println("<html><body>");
+		            //한줄씩 읽는 메서드 
+		String str = reader.readLine();
+		
+		while(str != null) {
+			out.println(str + "<br>");
+			str = reader.readLine();
+		}
+		
+		reader.close();
+		
+		out.println("</body></html>");
 	}
 
 }
