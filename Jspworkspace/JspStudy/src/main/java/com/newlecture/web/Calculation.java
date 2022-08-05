25eb1a4bd1ae3ba62ade4fc85c6b496d7797d893
JaeYeon Kim <105400221+Sanic00@users.noreply.github.com>
2022-08-05 19:49:08 +0900

    Add files via upload

---
+++ Jspworkspace/JspStudy/src/main/java/com/newlecture/web/Calculation.java
@@ -0,0 +1,30 @@
+package com.newlecture.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Calculation")
+public class Calculation extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		int num1 = Integer.parseInt(request.getParameter("x"));
+		int num2 = Integer.parseInt(request.getParameter("y"));
+		
+		int sum = num1 + num2;
+		sum = Integer.parseInt(request.getParameter("계산"));		
+		out.println(sum);
+		
+	}
+}
