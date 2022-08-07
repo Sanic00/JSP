45fd255a81fb69dba03ce48b034548e69f646836
JaeYeon Kim <105400221+Sanic00@users.noreply.github.com>
2022-08-07 15:36:20 +0900

    Add files via upload

--- Jspworkspace/JspStudy/src/main/java/com/newlecture/web/Add2.java
+++ Jspworkspace/JspStudy/src/main/java/com/newlecture/web/Add2.java
@@ -7,29 +7,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//@WebServlet("/add")
-public class Add extends HttpServlet {
+@WebServlet("/add")
+public class Add2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
 		
+		String num1 = request.getParameter("x");
+		String num2 = request.getParameter("y");
 		
-		String x_ = request.getParameter("x"); //임시변수로 받음
-		String y_ = request.getParameter("y");
-		
-		//밑에 두 변수는 빈문자열이 못오도록 0으로 초기화
 		int x = 0;
 		int y = 0;
 		
-		if(!x_.equals("")) x = Integer.parseInt(x_);
-		if(!y_.equals("")) y = Integer.parseInt(y_);
+		if(!num1.equals(""))
+			x = Integer.parseInt(num1);
 		
-		int result = x + y;
+		if(!num2.equals(""))
+			y = Integer.parseInt(num2);
 		
-		response.getWriter().printf("reuslt is %d\n", result);
+		int sum = x+y;
+		int minus = x - y;
+		response.getWriter().print("두 수의 합은"+ sum +"입니다.");
+		response.getWriter().print("두 수의 뺄셈은"+ minus +"입니다.");
+		
+		
 	}
 
 }
